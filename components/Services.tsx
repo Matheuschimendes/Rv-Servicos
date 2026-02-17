0ce5b218a22878e8c64cc7bab0dbe85dee77fbbf
Matheus Chimendes <chimendes2013@outlook.com>
2026-02-17 17:22:54 -0300

    ajuste de foto paginas de servicos

--- components/Services.tsx
+++ components/Services.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 
 import sitePreview from '../assets/images/site-preview.png';
+import medicos2 from '../assets/images/medicos2.jpeg';
 
 const services = [
   {
@@ -26,7 +27,7 @@ const services = [
     title: 'Preceptoria Médica',
     desc: 'Foco em instituições de ensino superior e formação de qualidade.',
     icon: 'M12 14l9-5-9-5-9 5 9 5z M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z',
-    image: '../assets/images/medicos2.jpeg'
+    image: medicos2
   },
   {
     title: 'Apoio Diagnóstico',
