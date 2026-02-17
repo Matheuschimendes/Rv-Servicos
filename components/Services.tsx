4e1e5a852f803685a018223dfcc441ccd403d493
Matheus Chimendes <chimendes2013@outlook.com>
2026-02-17 17:09:19 -0300

    ajuste de foto paginas de servicos

--- components/Services.tsx
+++ components/Services.tsx
@@ -26,7 +26,7 @@ const services = [
     title: 'Preceptoria Médica',
     desc: 'Foco em instituições de ensino superior e formação de qualidade.',
     icon: 'M12 14l9-5-9-5-9 5 9 5z M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z',
-    image: 'https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?auto=format&fit=crop&w=900&q=80'
+    image: '/assets/images/medicos2.jpeg'
   },
   {
     title: 'Apoio Diagnóstico',
